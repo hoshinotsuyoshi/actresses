b4599ad53e7f56c44110d919b407ed1e447d9a81
root <root@www33138u.sakura.ne.jp>
2013-07-14 23:41:45 +0900

    define site name

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
@@ -13,6 +14,7 @@ Bundler.require(:default, Rails.env)
 
 module EroRailsReplacerDbDev
   class Application < Rails::Application
+    SiteName = "厳選女優画像"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
