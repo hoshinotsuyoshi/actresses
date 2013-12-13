ace7d40a323226796403aef69fcc68d691c0dc15
hoshinotsuyosh <guitarpopnot330@gmail.com>
2013-12-14 00:06:44 +0900

    Add scripts for db_batch

---
+++ bin/mongo_dump.sh
@@ -0,0 +1,2 @@
+#dump production
+#mongodump -h dharma.mongohq.com:10082 --db app16903621 -u <username> -p <password>
---
+++ bin/mongo_restore.sh
@@ -0,0 +1,6 @@
+# restore to production
+# mongorestore -h dharma.mongohq.com:10082 --db app16903621 -u<username> -p<password> ./dump/actress_app_dev
+
+# restore to local
+# mongorestore -h localhost:27017 --db actress_app_dev  ./dump/actress_app_dev
+
