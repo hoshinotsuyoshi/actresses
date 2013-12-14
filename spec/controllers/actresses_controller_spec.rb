d7bfdccd79bb08089ef6fbc93f0459d8d3d5da89
hoshinotsuyosh <guitarpopnot330@gmail.com>
2013-12-14 15:15:39 +0900

    Enable to pass test (actress_controller_spec.rb)

--- spec/controllers/actresses_controller_spec.rb
+++ spec/controllers/actresses_controller_spec.rb
@@ -19,6 +19,9 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe ActressesController do
+  before{
+    Actress.delete_all
+  }
 
   # This should return the minimal set of attributes required to create a valid
   # Actress. As you add validations to Actress, be sure to
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -19,5 +18,5 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = 'random'
+  # config.order = 'random'
 end
