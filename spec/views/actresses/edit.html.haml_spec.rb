1e4a2dc89de44d3f36aa9f9d01b02a01784515bc
hoshinotsuyosh <guitarpopnot330@gmail.com>
2013-12-14 16:59:48 +0900

    Enable to pass test (spec/views)

--- spec/views/actresses/edit.html.haml_spec.rb
+++ spec/views/actresses/edit.html.haml_spec.rb
@@ -15,7 +15,7 @@ describe "actresses/edit" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", actress_path(@actress), "post" do
       assert_select "input#actress_name[name=?]", "actress[name]"
-      assert_select "input#actress_text[name=?]", "actress[text]"
+      assert_select "textarea#actress_text[name=?]", "actress[text]"
       assert_select "input#actress_display[name=?]", "actress[display]"
     end
   end
