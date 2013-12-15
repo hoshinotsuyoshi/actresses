2c96f456129001d2f38140607741e7b21012b29b
hoshinotsuyosh <guitarpopnot330@gmail.com>
2013-12-15 20:08:55 +0900

    Enable to pass test (spec/models/photo)

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
+describe Photo do
+  it { should have_fields(:path, :url, :secure_url, :big_url).of_type(String) }
+  it { should belong_to(:actress)}
+  it { should have_fields(:release_date).of_type(DateTime) }
+  it { should have_fields(:point).of_type(Integer).with_default_value_of 0 }
+
+  #scope specs
+  it { expect(Photo.released  .selector["release_date"]["$lte"].class).to  eq(Time)} #FIXME
+  it { expect(Photo.unreleased.selector["release_date"]["$gt" ].class).to  eq(Time)} #FIXME
+end
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #FIXME? (we can use stub_chain in order to use should_receive?)
+  it "Photo#table" do
+    Photo.stub_chain(:group, :map)
+    Photo.table
+  end
 end
