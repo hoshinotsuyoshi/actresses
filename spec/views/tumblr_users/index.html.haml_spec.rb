1140464d9b95a22136f4ed53a266fa0b02a2c5da
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-01-21 22:55:28 +0900

    Remove false assertion

--- spec/views/tumblr_users/index.html.haml_spec.rb
+++ spec/views/tumblr_users/index.html.haml_spec.rb
@@ -27,9 +27,6 @@ describe "tumblr_users/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Host".to_s, :count => 2
     assert_select "tr>td", :text => "Consumer Key".to_s, :count => 2
-    assert_select "tr>td", :text => "Consumer Secret".to_s, :count => 2
-    assert_select "tr>td", :text => "Oauth Token".to_s, :count => 2
-    assert_select "tr>td", :text => "Oauth Token Secret".to_s, :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
   end
 end
