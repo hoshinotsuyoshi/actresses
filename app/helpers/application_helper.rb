93fe94d31576086e6b8443e4edad4300f6adc512
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-01-28 19:34:54 +0900

    Adjust for request spec test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def sp?
     ua = request.env["HTTP_USER_AGENT"]
+    return false if ua.nil? # for request-test
     ua.include?('Mobile') || ua.include?('Android')
   end
 end
